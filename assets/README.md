93bf11cf010267a1e1f8e8e411fb9a72c6d07e49
Riley19James <rikennedy@live.com>
2023-05-29 08:56:54 -0300

    css edit
    Changed format of css, fixed spacing between buttons to make more user friendly

---
+++ assets/README.md
@@ -0,0 +1,34 @@
+# Ultimate Fighter Championship Quiz
+
+A quiz game that tests your knowledge about the Ultimate Fighter Championship (UFC).
+
+## Description
+
+The Ultimate Fighter Championship Quiz is a web-based game that presents you with six questions about the UFC. You have 60 seconds to answer each question. For every incorrect answer, 10 seconds are subtracted from the timer. At the end of the quiz, you can save your score by entering your initials. The game also displays the high score achieved so far.
+
+## Instructions
+
+1. Click the "Start Quiz" button to begin.
+2. Read each question and choose an answer from the provided choices.
+3. If your answer is correct, the next question will be displayed.
+4. If your answer is incorrect, 10 seconds will be deducted from the timer, and the next question will be shown.
+5. The quiz ends when you have answered all the questions or when the timer reaches 0.
+6. After completing the quiz, enter your initials in the input field and click "Save Score" to record your score.
+7. The high score achieved will be displayed in the "High Scores" section.
+
+## Installation
+
+1. Launch Quiz from HTML Link - 
+
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+
+
+
+
+
+
--- assets/script.js
+++ assets/script.js
@@ -1,52 +1,63 @@
-const quizContainer = document.getElementById('quiz-container');
-const questionElement = document.getElementById('question');
-const choicesElement = document.getElementById('choices');
-const timerElement = document.getElementById('time');
-const startButton = document.getElementById('start-btn');
-const gameOverContainer = document.getElementById('game-over');
-const scoreForm = document.getElementById('score-form');
-const initialsInput = document.getElementById('initials');
-const highScoreText = document.getElementById('high-score-text');
+const quizContainer = document.getElementById("quiz-container");
+const questionElement = document.getElementById("question");
+const choicesElement = document.getElementById("choices");
+const timerElement = document.getElementById("time");
+const startButton = document.getElementById("start-btn");
+const gameOverContainer = document.getElementById("game-over");
+const scoreForm = document.getElementById("score-form");
+const initialsInput = document.getElementById("initials");
+const highScoreText = document.getElementById("high-score-text");
+const highScoreList = document.getElementById("high-score-list");
 
 let currentQuestionIndex = 0;
 let time = 60;
 let timerId;
 
-// Define your quiz questions and answers as an array of objects
 const quizQuestions = [
   {
-    question: 'Question 1: Conor McGregor is or was a holder of which UFC record?',
-    choices: ['Fastest title fight knockout', 'Most fights won', 'Most submissions', 'Most knockouts'],
-    correctAnswer: 'Fastest title fight knockout'
+    question:
+      "Question 1: Conor McGregor is or was a holder of which UFC record?",
+    choices: [
+      "Fastest title fight knockout",
+      "Most fights won",
+      "Most submissions",
+      "Most knockouts",
+    ],
+    correctAnswer: "Fastest title fight knockout",
   },
   {
-    question: 'Question 2: What is the shape of the UFC ring?',
-    choices: ['Circle', 'Octagon', 'Square', 'Triangle'],
-    correctAnswer: 'Octagon'
+    question: "Question 2: What is the shape of the UFC ring?",
+    choices: ["Circle", "Octagon", "Square", "Triangle"],
+    correctAnswer: "Octagon",
   },
   {
-    question: 'Question 3: Who won UFC 1 on November 12, 1993?',
-    choices: ['Dan Severn', 'Frank Mir', 'Royce Gracie ', 'Conor McGregor'],
-    correctAnswer: 'Royce Gracie '
+    question: "Question 3: Who won UFC 1 on November 12, 1993?",
+    choices: ["Dan Severn", "Frank Mir", "Royce Gracie ", "Conor McGregor"],
+    correctAnswer: "Royce Gracie ",
   },
   {
-    question: 'Question 4: Who is the UFC President',
-    choices: ['Vince McMahon', 'Steve Jobs', 'Elon Musk', 'Dana White'],
-    correctAnswer: 'Dana White'
+    question: "Question 4: Who is the UFC President",
+    choices: ["Vince McMahon", "Steve Jobs", "Elon Musk", "Dana White"],
+    correctAnswer: "Dana White",
   },
   {
     question: 'Question 5: Also Known as "The Diamond"',
-    choices: ['Dustin Poirier', 'Jose Aldo', 'Ken Shamrock', 'Tito Ortiz'],
-    correctAnswer: 'Dustin Poirier'
+    choices: ["Dustin Poirier", "Jose Aldo", "Ken Shamrock", "Tito Ortiz"],
+    correctAnswer: "Dustin Poirier",
   },
   {
-    question: 'Question 6: Where is the UFC Headquarters?',
-    choices: ['Saint John, Newbrunswick', 'Las Vegas, Nevada', 'Washington, D.C', 'Rome'],
-    correctAnswer: 'Las Vegas, Nevada'
+    question: "Question 6: Where is the UFC Headquarters?",
+    choices: [
+      "Saint John, Newbrunswick",
+      "Las Vegas, Nevada",
+      "Washington, D.C",
+      "Rome",
+    ],
+    correctAnswer: "Las Vegas, Nevada",
   },
 ];
 
-startButton.addEventListener('click', startQuiz);
+startButton.addEventListener("click", startQuiz);
 
 function startQuiz() {
   startButton.disabled = true;
@@ -58,15 +69,15 @@ function showQuestion() {
   const currentQuestion = quizQuestions[currentQuestionIndex];
   questionElement.textContent = currentQuestion.question;
 
-  choicesElement.innerHTML = '';
-  currentQuestion.choices.forEach(choice => {
-    const li = document.createElement('li');
-    const button = document.createElement('button');
+  choicesElement.innerHTML = "";
+  currentQuestion.choices.forEach((choice) => {
+    const li = document.createElement("li");
+    const button = document.createElement("button");
     button.textContent = choice;
     li.appendChild(button);
     choicesElement.appendChild(li);
 
-    button.addEventListener('click', () => {
+    button.addEventListener("click", () => {
       checkAnswer(button.textContent);
     });
   });
@@ -75,17 +86,14 @@ function showQuestion() {
 function checkAnswer(answer) {
   const currentQuestion = quizQuestions[currentQuestionIndex];
   if (answer === currentQuestion.correctAnswer) {
-    // Answer is correct
     currentQuestionIndex++;
     if (currentQuestionIndex === quizQuestions.length) {
-      // All questions answered
       endQuiz();
     } else {
       showQuestion();
     }
   } else {
-    // Answer is incorrect
-    time -= 10; // Subtract 10 seconds from the timer
+    time -= 10;
   }
 }
 
@@ -97,44 +105,60 @@ function updateTimer() {
   }
 }
 
-
 function endQuiz() {
   clearInterval(timerId);
-  quizContainer.classList.add('hide');
-  gameOverContainer.classList.remove('hide');
-  scoreForm.addEventListener('submit', saveScore);
+  quizContainer.classList.add("hide");
+  gameOverContainer.classList.remove("hide");
+  scoreForm.addEventListener("submit", saveScore);
 
-  // Display high score
   const highScore = getHighScore();
-  if (highScore) {
+  if (highScore && highScore.initials && highScore.score) {
     highScoreText.textContent = `High Score: ${highScore.initials} - ${highScore.score}`;
+  } else {
+    highScoreText.textContent = "High Score: No high score yet.";
   }
+
+  const userScore = document.createElement("p");
+  userScore.textContent = `Your Score: ${time} - Initials: `;
+  const userInitials = document.createElement("span");
+  userInitials.setAttribute("id", "user-initials");
+  userScore.appendChild(userInitials);
+  gameOverContainer.appendChild(userScore);
+
+  initialsInput.disabled = false;
+  initialsInput.value = "";
+  initialsInput.focus();
+  scoreForm.querySelector("button").disabled = false;
 }
 
-
+function getHighScore() {
+  const highScore = localStorage.getItem("highScore");
+  return highScore ? JSON.parse(highScore) : null;
+}
 
 function saveScore(event) {
   event.preventDefault();
   const initials = initialsInput.value;
   const score = time;
 
-  // Save the score
   const highScore = getHighScore();
   if (!highScore || score > highScore.score) {
     const newHighScore = {
       initials: initials,
-      score: score
+      score: score,
     };
-    localStorage.setItem('highScore', JSON.stringify(newHighScore));
+    localStorage.setItem("highScore", JSON.stringify(newHighScore));
     highScoreText.textContent = `High Score: ${initials} - ${score}`;
   }
 
-  // Disable the form after saving the score
-  initialsInput.disabled = true;
-  scoreForm.querySelector('button').disabled = true;
-}
+  const scoreText = document.getElementById("score-text");
+  const initialsText = document.getElementById("initials-text");
+  scoreText.textContent = score;
+  initialsText.textContent = initials;
 
-function getHighScore() {
-  const highScoreData = localStorage.getItem('highScore');
-  return highScoreData ? JSON.parse(highScoreData) : null;
+  scoreForm.classList.add("hide");
+  document.getElementById("user-score").classList.remove("hide");
+
+  initialsInput.disabled = true;
+  scoreForm.querySelector("button").disabled = true;
 }
